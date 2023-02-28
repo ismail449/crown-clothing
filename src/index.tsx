f454af064b237c00e6ad42ac3a1efb30f939b4da
ismail khaled <esmael449@stud.fci-cu.edu.eg>
2023-02-28 18:04:26 +0200

    removed test file

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
+
 import { App } from "./App";
 import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 import reportWebVitals from "./reportWebVitals";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(

1b5eb3d826a6e5300d5e4b4fd1a6aacc65bf410a
ismail khaled <esmael449@stud.fci-cu.edu.eg>
2023-02-28 18:21:20 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import { App } from "./App";
+import { App } from "./app";
 import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";

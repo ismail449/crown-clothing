191593de10a1f7904a67ea489fbc8dfe59bae180
ismail khaled <esmael449@stud.fci-cu.edu.eg>
2023-03-04 19:01:28 +0200

    added redux persistor to persist the cart

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,27 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blackList: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleware = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleware));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
